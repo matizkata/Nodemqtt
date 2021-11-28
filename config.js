b852d8bb872df8e7531901b3838c4708e38a91cc
matizkata <67799505+matizkata@users.noreply.github.com>
2021-11-28 12:10:37 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'facebookAuth': {
-		'clientID':  '337956471472028',
-		'clientSecret':  'a068d2e3ef1924617513d55684a9f780',
+		'clientID':  '************',
+		'clientSecret':  '****************',
     'callbackURL': 'https://mklocalbroker.local:3000/auth/facebook/callback'
 	}
 }
